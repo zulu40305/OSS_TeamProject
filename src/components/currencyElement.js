d5fb9a8598632781bce39cad139a1b9b6a08b0ad
zulu40305 <zulu40305@gmail.com>
2024-12-08 19:31:18 +0900

    bug fixed

--- src/components/currencyElement.js
+++ src/components/currencyElement.js
@@ -1,5 +1,4 @@
 import styles from './currencyElement.module.css';
-import { ReactCountryFlag } from '@fadi-ui/react-country-flag';
 import Flag from 'react-flagkit';
 
 export default function CurrencyElement(props) {
@@ -8,16 +7,6 @@ export default function CurrencyElement(props) {
     <div key={props.data.cur_unit} className={styles.currency_element_container} onClick={() => props.setTarget(props.data)}>
       <div className={styles.flag_container}>
         {
-          props.iso_code === "EU" ? 
-          <>
-            <img 
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Flag_of_Europe.svg/225px-Flag_of_Europe.svg.png"
-              alt=""
-              style={{width: "25px", height: "19px"}} 
-            />
-            {`${props.data.cur_nm}(${props.data.cur_unit})`}
-          </>
-          :
           <>
             <Flag country={props.iso_code} size={25} />
             {`${props.data.cur_nm}(${props.data.cur_unit})`}
